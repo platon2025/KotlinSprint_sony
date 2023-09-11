6eefefd22f88b63fc97d49a90a2c5745d324661a
platon2025 <platon@enod.xyz>
2023-09-11 23:04:53 +0500

    KS-03-5 -- Parse chess move string

--- src/main/kotlin/lesson_03/lesson_03-task_5.kt
+++ src/main/kotlin/lesson_03/lesson_03-task_5.kt
@@ -2,3 +2,13 @@ package lesson_03
 
 /** Created by Platon2025 */
 
+fun main() {
+    val input =  "D2-D4;0"
+    val from = input.substring(0,2)
+    val to = input.substring(3,5)
+    val step = input.substringAfter(';')
+
+    println("From :\t$from")
+    println("To :\t$to")
+    println("Step :\t$step")
+}
