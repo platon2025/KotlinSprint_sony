aa8e62454d3893410e8d6c0edddf1df9a9fea61e
platon2025 <platon@enod.xyz>
2023-09-11 23:20:45 +0500

    KS-04-2 -- Cargo 'Average'

--- src/main/kotlin/lesson_04/lesson_04-task_2.kt
+++ src/main/kotlin/lesson_04/lesson_04-task_2.kt
@@ -2,3 +2,19 @@ package lesson_04
 
 /** Created by Platon2025 */
 
+fun main() {
+
+    val minWeight = 35
+    val maxWeight = 100
+    val maxVolume = 100
+    var cargoWeight: Int
+    var cargoVolume: Int
+
+    cargoWeight = 20
+    cargoVolume = 80
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${cargoWeight >= minWeight && cargoWeight <= maxWeight && cargoVolume <= maxVolume}")
+
+    cargoWeight = 50
+    cargoVolume = 100
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${cargoWeight >= minWeight && cargoWeight <= maxWeight && cargoVolume <= maxVolume}")
+}
