decde066e1c4d8b456cde28026e8401ec5f62568
platon2025 <platon@enod.xyz>
2023-09-11 22:59:52 +0500

    KS-03-4 -- Chess board

--- src/main/kotlin/lesson_03/lesson_03-task_4.kt
+++ src/main/kotlin/lesson_03/lesson_03-task_4.kt
@@ -2,3 +2,14 @@ package lesson_03
 
 /** Created by Platon2025 */
 
+fun main(){
+    var count: Int = 1
+    var fromCell: String = "E2"
+    var toCell: String = "E4"
+    println("$fromCell-$toCell;$count")
+
+    count = 3 // такой ход не может быть вторым в партии
+    fromCell = "D2"
+    toCell = "D3"
+    println("$fromCell-$toCell;$count")
+}
