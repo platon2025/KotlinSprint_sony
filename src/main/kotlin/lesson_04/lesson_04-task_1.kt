fc0a4681462b37017ba596f599e2803cec346c71
platon2025 <platon@enod.xyz>
2023-09-11 23:15:04 +0500

    KS-04-1 -- Reserve tables

--- src/main/kotlin/lesson_04/lesson_04-task_1.kt
+++ src/main/kotlin/lesson_04/lesson_04-task_1.kt
@@ -2,3 +2,14 @@ package lesson_04
 
 /** Created by Platon2025 */
 
+fun main() {
+    val totalTables = 13
+
+    val reservedTablesToday = 13
+    val reservedTablesTomorrow = 13 - 4
+
+    val freeTablesToday = totalTables > reservedTablesToday
+    val freeTablesTomorrow = totalTables > reservedTablesTomorrow
+
+    println("Доступность столиков на сегодня: $freeTablesToday\nДоступность столиков на завтра: $freeTablesTomorrow")
+}
