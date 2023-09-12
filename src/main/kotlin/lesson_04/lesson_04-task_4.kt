8dfa046d5379445a095c9e018d683d5b9267f8db
platon2025 <platon@enod.xyz>
2023-09-12 15:39:50 +0500

    KS-04-4 -- List of exercises

--- src/main/kotlin/lesson_04/lesson_04-task_4.kt
+++ src/main/kotlin/lesson_04/lesson_04-task_4.kt
@@ -2,3 +2,14 @@ package lesson_04
 
 /** Created by Platon2025 */
 
+fun main() {
+    val dayNumber = 1
+
+    val oddDay = 1
+    val evenDay = 0
+    val exercises = mutableMapOf("Упражнения для рук" to oddDay, "Упражнения для ног" to evenDay)
+    exercises.put("Упражнения для спины", evenDay)
+    exercises.put("Упражнения для пресса", oddDay)
+
+    exercises.forEach { (exs, day) -> println("$exs:\t${ dayNumber % 2 == day}") }
+}
