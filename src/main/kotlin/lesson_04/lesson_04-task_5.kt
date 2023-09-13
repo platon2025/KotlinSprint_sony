6bd5310de3a643bb263d4dbc5161f2f44b69a208
platon2025 <platon@enod.xyz>
2023-09-13 14:27:40 +0500

    KS-05-1 -- Bot-control

--- src/main/kotlin/lesson_04/lesson_04-task_5.kt
+++ src/main/kotlin/lesson_04/lesson_04-task_5.kt
@@ -2,37 +2,43 @@ package lesson_04
 
 /** Created by Platon2025 */
 
+const val MIN_CREW_NUM = 55
+const val MAX_CREW_NUM = 70
+const val RECOMMENDED_CREW_NUM = 70
+const val MIN_PROVISION = 50
+
 fun main() {
 
-    var input: String?
+    var input: String
     val crew: Int
     val provision: Int
 
     println("Есть ли повреждения на корпусе корабля? Да или Нет")
-    val damage = readLine() == "Да"
-    println("Повреждения: $damage\n")
+    val isDamaged = readln().lowercase() == "да"
+    println("Повреждения: $isDamaged\n")
 
     println("Укажите число экипажа")
-    input = readLine()
-    if( input == "" ) crew = 0
-    else crew = input?.toInt()!!
+    input = readln()
+    if (input.isEmpty()) crew = 0
+    else crew = input.toInt()
     println("Экипаж в составе: $crew чел.\n")
 
     println("Сколько ящиков шампанского на борту?")
-    input = readLine()
-    if( input == "" ) provision = 0
-    else provision = input?.toInt()!!
+    input = readln()
+    if (input.isEmpty()) provision = 0
+    else provision = input.toInt()
     println("Провизии $provision ящ.\n")
 
     println("Погода благоприятная? Да или Нет")
-    val weather = readLine() == "Да"
+    val weather = readln().lowercase() == "да"
     println("Погода: $weather\n")
 
     var departApprove = "не разрешен"
 
-    if( !damage && crew in 55..70 && provision > 50 ) departApprove = "разрешен"
-    if( crew == 70 && weather && provision > 50) departApprove = "разрешен"
+    if (!isDamaged && crew in MIN_CREW_NUM..MAX_CREW_NUM && provision > MIN_PROVISION
+        || crew == RECOMMENDED_CREW_NUM && weather && provision > MIN_PROVISION
+    ) departApprove = "разрешен"
 
-    println ("Комиссия все проверила и постановила : Кораблю $departApprove выход в море.")
+    println("Комиссия все проверила и постановила : Кораблю $departApprove выход в море.")
 
 }
