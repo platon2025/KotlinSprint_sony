356a3e937cac8e7da5e186af0cd088cffc936fc1
platon2025 <platon@enod.xyz>
2023-09-13 14:01:47 +0500

    KS-05-1 -- Bot-control

--- src/main/kotlin/lesson_05/lesson_05-task_1.kt
+++ src/main/kotlin/lesson_05/lesson_05-task_1.kt
@@ -2,3 +2,28 @@ package lesson_05
 
 /** Created by Platon2025 */
 
+fun main() {
+    val input: String
+    val message: String
+    val task = "Напиши математический пример на сложение двух положительных чисел (без пробелов)\nЦифры могут быть целыми или десятичными дробями"
+    val example: List<String>
+    var result = false
+
+    println("Бот не сможет это сделать, а ты сможешь :\n$task")
+
+    input = readln()
+    if (!input.isEmpty()) {
+        example = input?.split('+', '=')
+        if (example.size == 3 && input.contains('+') && input.contains('=')) {
+            if (example[0].toFloat() + example[1].toFloat() == example[2].toFloat()) {
+                result = true
+            }
+        }
+    }
+
+    if (result) message = "\"Добро пожаловать!\""
+    else message = "\"Доступ запрещен!\""
+
+    println(message)
+
+}
