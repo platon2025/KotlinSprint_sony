ec97c551ef17cd1c22c664902a2afb5d9ad38e75
platon2025 <platon@enod.xyz>
2023-09-14 16:16:53 +0500

    KS-05-5 -- LOTTO 2 of 100 _improved _update

--- src/main/kotlin/lesson_05/lesson_05-task_3.kt
+++ src/main/kotlin/lesson_05/lesson_05-task_3.kt
@@ -7,7 +7,7 @@ fun main() {
     val secret2 = 54
 
     val secret = mutableSetOf(secret1, secret2)
-    var finalResult = 0
+    var finalScore = 0
 
     println("Загадано два числа от 1 до 100\nУгадайте оба числа и получите ГЛАВНЫЙ ПРИЗ !")
 
@@ -16,7 +16,7 @@ fun main() {
     var result = checkBet(bet1, secret)
     if (result) {
         secret.remove(bet1)
-        finalResult++
+        finalScore++
     }
     println(getCommentOnResult(result))
 
@@ -25,26 +25,19 @@ fun main() {
     result = checkBet(bet2, secret)
     if (result) {
         secret.remove(bet2)
-        finalResult++
+        finalScore++
     }
     println(getCommentOnResult(result))
 
-    val finalComment = when (finalResult) {
+    val finalComment = when (finalScore) {
         2 -> "Поздравляем! Вы выиграли главный приз!"
         1 -> "Вы выиграли утешительный приз!"
         else -> "Неудача! Крутите барабан!"
     }
 
     println(finalComment)
-
-
 }
 
-fun checkBet(bet: Int, secret: Set<Int>): Boolean {
-    return secret.contains(bet)
-}
+fun checkBet(bet: Int, secret: Set<Int>): Boolean = secret.contains(bet)
 
-fun getCommentOnResult(result: Boolean): String {
-    if (result) return "В яблочко !"
-    else return "Мимо"
-}
+fun getCommentOnResult(result: Boolean): String = if (result) "В яблочко !" else "Мимо"
