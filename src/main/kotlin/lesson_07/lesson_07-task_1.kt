e5a3cbd61f655bc24967890d604dec1e8438f70d
platon2025 <platon@enod.xyz>
2023-09-17 00:42:18 +0500

    KS-07-1 -- password generator

--- src/main/kotlin/lesson_07/lesson_07-task_1.kt
+++ src/main/kotlin/lesson_07/lesson_07-task_1.kt
@@ -2,3 +2,20 @@ package lesson_07
 
 /** Created by Platon2025 */
 
+fun main() {
+
+    val passwordLength = 6
+    val string = "0123456789absdefghijklmnopqrstuvwxyz"
+    var pos: Int //позиция символа в строке
+
+    var password = ""
+
+    /*  не понял, для чего требуется создавать отдельную переменную для каждого символа,
+        и как собственно это организовать. Сделал без этого.  */
+
+    for (i in 1..passwordLength) {
+        pos = (0 until string.length ).random()
+        password += string[pos]
+    }
+    println(password)
+}
