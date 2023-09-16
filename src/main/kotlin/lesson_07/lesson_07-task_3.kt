0d6476605d1eb56c69832d2f1a110a9357928fda
platon2025 <platon@enod.xyz>
2023-09-17 01:31:57 +0500

    KS-07-3 -- even number range

--- src/main/kotlin/lesson_07/lesson_07-task_3.kt
+++ src/main/kotlin/lesson_07/lesson_07-task_3.kt
@@ -2,3 +2,16 @@ package lesson_07
 
 /** Created by Platon2025 */
 
+fun main() {
+    var outputString = ""
+
+    println("Введите число")
+    val maxNum = readln().toInt()
+    for (num in 0 .. maxNum step 2) {
+        outputString += "${num.toString()},"
+    }
+    outputString = outputString.dropLast(1)
+    println("Все четные числа от 0 до $maxNum:\n$outputString")
+
+
+}
