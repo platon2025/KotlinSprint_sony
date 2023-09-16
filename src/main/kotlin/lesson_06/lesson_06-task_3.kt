9b948787fe546a59b39fb7ec2c43cd710a6f0535
platon2025 <platon@enod.xyz>
2023-09-16 22:32:45 +0500

    KS-06-2 -- Timer : set/count

--- src/main/kotlin/lesson_06/lesson_06-task_3.kt
+++ src/main/kotlin/lesson_06/lesson_06-task_3.kt
@@ -2,3 +2,14 @@ package lesson_06
 
 /** Created by Platon2025 */
 
+fun main() {
+    println("На сколько секунд ставим таймер?\nВведите число :")
+    val time = readln().toInt()
+    var count = time
+    while (count > 0) {
+        println("Осталось секунд: $count")
+        Thread.sleep(1000)
+        count--
+    }
+    println("Время вышло")
+}
