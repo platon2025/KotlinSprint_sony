1adc56a04639e4618662d7ce263f809a3e003d5b
platon2025 <platon@enod.xyz>
2023-09-16 22:59:24 +0500

    KS-06-4 -- Guess the number

--- src/main/kotlin/lesson_06/lesson_06-task_4.kt
+++ src/main/kotlin/lesson_06/lesson_06-task_4.kt
@@ -1,4 +1,25 @@
 package lesson_06
 
 /** Created by Platon2025 */
-
+const val MIN_NUM = 1
+const val MAX_NUM = 9
+fun main() {
+    val secret = (MIN_NUM..MAX_NUM).random()
+    var counter = 5
+    var success = false
+    var enter: Int
+    println("Загадано число от $MIN_NUM до $MAX_NUM")
+    println("У тебя $counter попыток чтобы его угадать")
+    println("Введи число :")
+    do {
+        enter = readln().toInt()
+        if (enter == secret) success = true
+        else {
+            counter--
+            if(counter > 0 ) println("Не-а. Осталось $counter попыток")
+            else println("Не удалось! Но расстраиваться не стоит! В следующий раз повезет обязательно!")
+        }
+    } while (counter > 0 && !success)
+    if (success) println("Бинго! Это была великолепная игра!")
+    else println("Было загадано число $secret")
+}
