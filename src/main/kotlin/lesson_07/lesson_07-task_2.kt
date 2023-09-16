f20d0a8dd48c00859e87aaa71401d9eae0c96004
platon2025 <platon@enod.xyz>
2023-09-17 01:13:42 +0500

    KS-07-2 -- dynamic access code

--- src/main/kotlin/lesson_07/lesson_07-task_2.kt
+++ src/main/kotlin/lesson_07/lesson_07-task_2.kt
@@ -2,3 +2,18 @@ package lesson_07
 
 /** Created by Platon2025 */
 
+fun main() {
+    var input: Int
+    var oneTimePassword: Int
+    val numberDiapasone = 1000..9999
+    var success = false
+
+    do {
+        oneTimePassword = numberDiapasone.random()
+        println("Ваш код авторизации : $oneTimePassword")
+        println("Введите код :")
+        input = readln().toInt()
+        if (input == oneTimePassword) success = true
+    } while (!success)
+    println("Добро пожаловать")
+}
