5d269b51441fb32cbeedd090bdc17eb18aa71212
platon2025 <platon@enod.xyz>
2023-09-16 22:07:28 +0500

    KS-06-1 -- Registration / Authorization

--- src/main/kotlin/lesson_06/lesson_06-task_1.kt
+++ src/main/kotlin/lesson_06/lesson_06-task_1.kt
@@ -2,3 +2,31 @@ package lesson_06
 
 /** Created by Platon2025 */
 
+fun main() {
+    var verify = false
+    val login: String
+    val password: String
+    var loginGiven: String
+    var passwordGiven: String
+
+    println("Регистрация нового пользователя")
+    println()
+    println("Придумайте логин :")
+    login = readln()
+    println("Логин $login")
+    println("Придумайте пароль :")
+    password = readln()
+    println("Отлично! Теперь вы можете авторизоваться.")
+    Thread.sleep(1000)
+    do {
+        println("Введите логин :")
+        loginGiven = readln()
+        println("Введите пароль: ")
+        passwordGiven = readln()
+        if (loginGiven == login && passwordGiven == password) {
+            verify = true
+        } else println("Пользователь с указанными данными не найден.\nПопробуйте ещё раз")
+    } while (!verify)
+
+    println("Авторизация прошла успешно")
+}
