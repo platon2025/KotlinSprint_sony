70bc2d6d8fc7719afb2737ba757227116cf132bc
platon2025 <platon@enod.xyz>
2023-09-17 01:34:52 +0500

    KS-07-3 -- even number range _update

--- src/main/kotlin/lesson_07/lesson_07-task_3.kt
+++ src/main/kotlin/lesson_07/lesson_07-task_3.kt
@@ -7,7 +7,7 @@ fun main() {
 
     println("Введите число")
     val maxNum = readln().toInt()
-    for (num in 0 .. maxNum step 2) {
+    for (num in 0..maxNum step 2) {
         outputString += "$num,"
     }
     outputString = outputString.dropLast(1)
