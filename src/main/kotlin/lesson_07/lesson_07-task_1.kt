037278feb5a81ba5ae37a508894526e31a0dec56
platon2025 <platon@enod.xyz>
2023-09-18 03:08:40 +0500

    KS-07-1 -- Password generator

--- src/main/kotlin/lesson_07/lesson_07-task_1.kt
+++ src/main/kotlin/lesson_07/lesson_07-task_1.kt
@@ -4,18 +4,31 @@ package lesson_07
 
 fun main() {
 
-    val passwordLength = 6
-    val string = "0123456789absdefghijklmnopqrstuvwxyz"
-    var pos: Int //позиция символа в строке
+    val rangeLetters = 'a' .. 'z'
+    val rangeNumbers = '0'..'9'
 
-    var password = ""
+    val p1 = rangeLetters.random().toString()
+    val p2 = rangeNumbers.random().toString()
+    val p3 = rangeLetters.random().toString()
+    val p4 = rangeNumbers.random().toString()
+    val p5 = rangeLetters.random().toString()
+    val p6 = rangeNumbers.random().toString()
 
-    /*  не понял, для чего требуется создавать отдельную переменную для каждого символа,
-        и как собственно это организовать. Сделал без этого.  */
+   val password = p1 + p2 + p3 + p4 + p5 + p6
 
-    for (i in 1..passwordLength) {
-        pos = (0 until string.length).random()
-        password += string[pos]
-    }
     println(password)
+
+
+//    val passwordLength = 6
+//    val string = "0123456789absdefghijklmnopqrstuvwxyz"
+//    var pos: Int //позиция символа в строке
+//
+//    var password = ""
+//
+//    for (i in 1..passwordLength) {
+//        pos = (0 until string.length).random()
+//        password += string[pos]
+//    }
+//    println(password)
+
 }
