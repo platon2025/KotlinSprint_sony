3766dc5609502eee2f882a38e8413cb787ff9b16
platon2025 <platon@enod.xyz>
2023-09-18 06:51:04 +0500

    KS-06-5 -- Bot control

--- src/main/kotlin/lesson_06/lesson_06-task_5.kt
+++ src/main/kotlin/lesson_06/lesson_06-task_5.kt
@@ -7,7 +7,6 @@ fun main() {
     var firstNum: Int
     var secondNum: Int
     var enter: Int
-    var success = false
     var counter = 3
 
     println("Чтобы пройти антибот-проверку, реши арифметический пример и введи ответ :")
@@ -17,13 +16,14 @@ fun main() {
         secondNum = (MIN_NUM..MAX_NUM).random()
         println("$firstNum + $secondNum = ")
         enter = readln().toInt()
-        if (firstNum + secondNum == enter) success = true
-        if (success) println("Добро пожаловать!")
-        else {
+        if (firstNum + secondNum == enter) {
+            println("Добро пожаловать!")
+            break
+        } else {
             counter--
             if (counter > 0) println("Давай еще раз попробуем")
+            else println("Доступ запрещен!")
         }
-    } while (!success && counter > 0)
-    if (!success) println("Доступ запрещен!")
+    } while (counter > 0)
 
 }
