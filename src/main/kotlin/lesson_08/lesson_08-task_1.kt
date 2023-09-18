24f64f08cb514f249aba1062b55528331d849a57
platon2025 <platon@enod.xyz>
2023-09-18 11:56:29 +0500

    KS-08-1 -- Advert views counter

--- src/main/kotlin/lesson_08/lesson_08-task_1.kt
+++ src/main/kotlin/lesson_08/lesson_08-task_1.kt
@@ -2,3 +2,22 @@ package lesson_08
 
 /** Created by Platon2025 */
 
+fun main() {
+    val viewsSunday = 100_500
+    val viewsMonday = 200_500
+    val viewsTuesday = 300_500
+    val viewsWedensday = 400_500
+    val viewsThirsday = 500_500
+    val viewsFriday = 600_500
+    val viewsSaturday = 700_500
+
+    val viewsWeek =
+        intArrayOf(viewsSunday, viewsMonday, viewsTuesday, viewsWedensday, viewsThirsday, viewsFriday, viewsSaturday)
+
+    var totalViews = 0
+    for (i in viewsWeek) {
+        totalViews += i
+    }
+
+    println("Общее количество просмотров за неделю : $totalViews")
+}
