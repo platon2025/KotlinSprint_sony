718bd5e556957d887bd851040144b5f63e92f744
platon2025 <platon@enod.xyz>
2023-09-18 07:19:58 +0500

    KS-07-4 -- Simple Timer

--- src/main/kotlin/lesson_07/lesson_07-task_4.kt
+++ src/main/kotlin/lesson_07/lesson_07-task_4.kt
@@ -2,3 +2,14 @@ package lesson_07
 
 /** Created by Platon2025 */
 
+fun main() {
+    println("Сколько секунд будем засекать? Введите число :")
+    var counter = readln().toInt()
+    for (i in 0 until counter) {
+        println("Осталось $counter секунд")
+        counter--
+        Thread.sleep(1000)
+    }
+    println("Время вышло")
+
+}
