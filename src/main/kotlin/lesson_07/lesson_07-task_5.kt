388a8a368cce3d7c4b719186759f819e568eefe4
platon2025 <platon@enod.xyz>
2023-09-19 15:04:08 +0500

    KS-07-5 -- Advanced password generator

--- src/main/kotlin/lesson_07/lesson_07-task_5.kt
+++ src/main/kotlin/lesson_07/lesson_07-task_5.kt
@@ -3,16 +3,13 @@ package lesson_07
 /** Created by Platon2025 */
 
 fun main() {
+    val chars = ('0'..'9') + ('A'..'Z') + ('a'..'z')
     println("Введите желаемую длину пароля : ")
     val passwordLength = readln().toInt()
-    val string = "0123456789absdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var pos: Int //позиция символа в строке
 
     var password = ""
-
     for (i in 1..passwordLength) {
-        pos = (0 until string.length).random()
-        password += string[pos]
+        password += chars.random()
     }
     println("Пароль : $password")
 }
