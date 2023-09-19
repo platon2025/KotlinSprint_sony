02af3d6dde90151becccfa1920d530550024cdb4
platon2025 <platon@enod.xyz>
2023-09-19 08:20:41 +0500

    KS-08-4 -- Recipe : Ingredient replacemant

--- src/main/kotlin/lesson_08/lesson_08-task_4.kt
+++ src/main/kotlin/lesson_08/lesson_08-task_4.kt
@@ -2,3 +2,25 @@ package lesson_08
 
 /** Created by Platon2025 */
 
+fun main() {
+
+    val contents = arrayOf("свёкла", "картофель", "морковь", "капуста", "масло")
+    var output: String
+
+    output = "Список ингридиентов : "
+    for (i in contents) output += "$i, "
+    println(output.substring(0, output.length - 2))
+
+    println("Какой ингридиент вы хотите заменить?")
+    val remove = readln()
+    if (!contents.contains(remove)) println("Ингредиента $remove нет в составе рецепта")
+    else {
+        println("На что вы хотели бы заменить $remove ?")
+        contents[contents.indexOf(remove)] = readln()
+
+        output = "Ингредиент заменён\nОбновленный состав ингредиентов: "
+        for (i in contents) output += "$i, "
+        println(output.substring(0, output.length - 2))
+    }
+
+}
