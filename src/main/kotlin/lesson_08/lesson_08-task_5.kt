7028a50a99006e1af3f9e1ff028b1469fad0ead8
platon2025 <platon@enod.xyz>
2023-09-19 08:47:50 +0500

    KS-08-5 -- Recipe : Dynamic adding ingredients

--- src/main/kotlin/lesson_08/lesson_08-task_5.kt
+++ src/main/kotlin/lesson_08/lesson_08-task_5.kt
@@ -2,3 +2,20 @@ package lesson_08
 
 /** Created by Platon2025 */
 
+fun main() {
+    println("Сколько ингредиентов в составе рецепта? Введите число :")
+    val ingredientQty = readln().toInt()
+    val contents = arrayOfNulls<String>(ingredientQty)
+
+    for (i in 0 until ingredientQty) {
+        if (i == 0) println("Введите первый ингредиент :")
+        else if (i == ingredientQty - 1) println("Введите последний ингредиент :")
+        else println("Введите следующий ингредиент :")
+        contents[i] = readln()
+    }
+
+    var output = "Ваш состав ингредиентов: "
+    for (i in contents) output += "$i, "
+    println(output.substring(0, output.length - 2))
+
+}
