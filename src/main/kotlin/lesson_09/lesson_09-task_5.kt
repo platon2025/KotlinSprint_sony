4f55edde7a12a116bd320b2540aa6bb1cab9e339
platon2025 <platon@enod.xyz>
2023-09-19 23:52:16 +0500

    KS-09-5 -- Ingredients non-repetitive, replaceFirstChar

--- src/main/kotlin/lesson_09/lesson_09-task_5.kt
+++ src/main/kotlin/lesson_09/lesson_09-task_5.kt
@@ -2,3 +2,29 @@ package lesson_09
 
 /** Created by Platon2025 */
 
+fun main() {
+    val qty = 5
+    var input: String
+    var output = ""
+    val list: MutableList<String> = mutableListOf()
+
+    println("Давайте введем $qty ингредиентов для вашего блюда")
+
+    for (i in 0 until qty) {
+        println("Введите ингредиент №${i + 1}")
+        input = readln().lowercase().trim()
+        while (input in list) {
+            println("Этот ингредиент уже есть в списке\nВведите другой:")
+            input = readln().lowercase().trim()
+        }
+        list.add(input)
+    }
+
+    list.sort()
+    list.forEach {
+        output += "$it, "
+    }
+    output = output.substring(0, output.length - 2).replaceFirstChar { it.uppercase() } + "."
+
+    println(output)
+}
