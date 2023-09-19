6acff3419e39b81d413a775974f80abce1cd5b11
platon2025 <platon@enod.xyz>
2023-09-19 22:30:36 +0500

    KS-09-2 -- Recipe - mutableListOf

--- src/main/kotlin/lesson_09/lesson_09-task_3.kt
+++ src/main/kotlin/lesson_09/lesson_09-task_3.kt
@@ -2,3 +2,15 @@ package lesson_09
 
 /** Created by Platon2025 */
 
+fun main() {
+    val ingredient = listOf("яйцо", "молоко", "сливочное масло")
+    val perPortion = listOf(2, 50, 15)
+    val unit = listOf("шт", "мл", "гр")
+
+    println("Сколько порций омлета хотите приготовить?\nВведите число :")
+    val qty = readln().toInt()
+    var output = "Для $qty порций вам понадобится: "
+    for (i in 0 until ingredient.size) output += "${ingredient[i]} - ${perPortion[i] * qty} ${unit[i]}, "
+
+    println(output.substring(0, output.length - 2))
+}
