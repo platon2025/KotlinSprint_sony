93cd9bb5505439f92b30c229a2ff4b7336aaa968
platon2025 <platon@enod.xyz>
2023-09-19 22:03:20 +0500

    KS-09-2 -- Recipe - mutableListOf

--- src/main/kotlin/lesson_09/lesson_09-task_2.kt
+++ src/main/kotlin/lesson_09/lesson_09-task_2.kt
@@ -2,3 +2,12 @@ package lesson_09
 
 /** Created by Platon2025 */
 
+fun main() {
+    val listOfIngredients = mutableListOf("мука", "сахар", "яйцо")
+    println("В рецепте есть базовые ингредиенты : $listOfIngredients\nЖелаете добавить ещё? ( Да / Нет)")
+    if (readln().lowercase() != "да") return
+    println("Какой ингредиент вы хотите добавить?")
+    listOfIngredients.add(readln())
+    println("Теперь в рецепте есть следующие ингредиенты: $listOfIngredients")
+
+}
