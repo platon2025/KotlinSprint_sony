aff7edf18a0e80fa0c7c8df5dd3803c88c284b30
platon2025 <platon@enod.xyz>
2023-09-20 12:53:26 +0500

    KS-09-2 -- Recipe - mutableListOf

--- src/main/kotlin/lesson_09/lesson_09-task_2.kt
+++ src/main/kotlin/lesson_09/lesson_09-task_2.kt
@@ -4,10 +4,10 @@ package lesson_09
 
 fun main() {
     val listOfIngredients = mutableListOf("мука", "сахар", "яйцо")
-    println("В рецепте есть базовые ингредиенты : $listOfIngredients\nЖелаете добавить ещё? ( Да / Нет)")
+    println("В рецепте есть базовые ингредиенты : ${listOfIngredients.joinToString(", ")}.\nЖелаете добавить ещё? ( Да / Нет)")
     if (readln().lowercase() != "да") return
     println("Какой ингредиент вы хотите добавить?")
     listOfIngredients.add(readln())
-    println("Теперь в рецепте есть следующие ингредиенты: $listOfIngredients")
+    println("Теперь в рецепте есть следующие ингредиенты: ${listOfIngredients.joinToString(", ")}.")
 
 }
