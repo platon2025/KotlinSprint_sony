244de33bc56a16ba4bd05bea4f48cc8e9c792f3b
platon2025 <platon@enod.xyz>
2023-09-20 13:19:13 +0500

    KS-09-3 -- Recipe - per portions

--- src/main/kotlin/lesson_09/lesson_09-task_3.kt
+++ src/main/kotlin/lesson_09/lesson_09-task_3.kt
@@ -3,14 +3,10 @@ package lesson_09
 /** Created by Platon2025 */
 
 fun main() {
-    val ingredient = listOf("яйцо", "молоко", "сливочное масло")
-    val perPortion = listOf(2, 50, 15)
-    val unit = listOf("шт", "мл", "гр")
-
+    val specification = listOf(2, 50, 15)
     println("Сколько порций омлета хотите приготовить?\nВведите число :")
     val qty = readln().toInt()
-    var output = "Для $qty порций вам понадобится: "
-    for (i in 0 until ingredient.size) output += "${ingredient[i]} - ${perPortion[i] * qty} ${unit[i]}, "
+    val customized = specification.map { it * qty }
+    println("Для $qty порций вам понадобится яиц – ${customized[0]}, молока – ${customized[1]}, сливочного масла – ${customized[2]}.")
 
-    println(output.substring(0, output.length - 2))
 }
