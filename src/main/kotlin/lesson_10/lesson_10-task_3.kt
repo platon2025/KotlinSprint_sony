d29994526751453d84ea1bff9194e5eb343f846b
platon2025 <platon@enod.xyz>
2023-09-21 20:08:46 +0500

    KS-10-3 -- Password from special chars

--- src/main/kotlin/lesson_10/lesson_10-task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10-task_3.kt
@@ -5,11 +5,16 @@ package lesson_10
 fun main() {
     println("Введите желаемую длину пароля :")
     println("Ваш пароль: ${generatePassword(readln().toInt())}")
+
 }
 
 fun generatePassword(length: Int): String {
-    val chars = ('0'..'9') + (' '..'/')
+    val chars1 = '0'..'9'
+    val chars2 = ' '..'/'
     var password = ""
-    for (i in 1..length) password += chars.random()
+    for (i in 1..length) {
+        if (i % 2 == 1) password += chars1.random()
+        else password += chars2.random()
+    }
     return password
 }
