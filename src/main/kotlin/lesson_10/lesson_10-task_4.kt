b34f3e09bd00cbeaa61bff8da3f8f9d1a05b52d0
platon2025 <platon@enod.xyz>
2023-09-21 09:51:56 +0500

    KS-10-4 -- Roll dices Advanced

--- src/main/kotlin/lesson_10/lesson_10-task_4.kt
+++ src/main/kotlin/lesson_10/lesson_10-task_4.kt
@@ -2,3 +2,38 @@ package lesson_10
 
 /** Created by Platon2025 */
 
+fun main() {
+    var humanWins = 0
+
+    do {
+        if (newRound()) humanWins++
+        println("Хотите бросить ещё? Да или Нет")
+    } while (readln().trim().lowercase() == "да")
+
+    println("Вы выиграли $humanWins раз")
+
+}
+
+fun rollDices(): List<Int> {
+    val range = 1..6
+    return listOf(range.random(), range.random())
+
+}
+
+fun newRound(): Boolean {
+
+    var roll = rollDices()
+    println("Компьютер бросил кубики. Результат ${roll[0]} + ${roll[1]}")
+    val machineResult = roll[0] + roll[1]
+    roll = rollDices()
+    Thread.sleep(1000)
+    println("Человек бросил кубики. Результат ${roll[0]} + ${roll[1]}")
+    val humanResult = roll[0] + roll[1]
+
+    if (humanResult > machineResult) println("У вас больше\n")
+    else if (humanResult < machineResult) println("У компьютера больше\n")
+    else println("Результат равный\n")
+
+    return humanResult > machineResult
+
+}
