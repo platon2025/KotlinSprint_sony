a3607d8c548c0f9434dabe7a1fa45515f10ee929
platon2025 <platon@enod.xyz>
2023-09-21 08:12:51 +0500

    KS-08-5 -- Dynamic adding ingredients : Empty array of given size

--- src/main/kotlin/lesson_08/lesson_08-task_5.kt
+++ src/main/kotlin/lesson_08/lesson_08-task_5.kt
@@ -7,6 +7,7 @@ fun main() {
     val ingredientQty = readln().toInt()
     val contents =
         Array<String>(ingredientQty) { "" }  //как в данном случае применить arrayOf я так и не разобрался. Сделал так.
+        //можно еще использовать emptyArray<String>() , но он должен быть var чтобы потом добавлять в него contents +=
 
     for (i in 0 until ingredientQty) {
         if (i == 0) println("Введите первый ингредиент :")
