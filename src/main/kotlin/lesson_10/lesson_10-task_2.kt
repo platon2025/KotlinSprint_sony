67968a3dc77c419e611d7fd79ba520dd0f1019c1
platon2025 <platon@enod.xyz>
2023-09-21 08:56:49 +0500

    KS-10-2 -- Password length validation

--- src/main/kotlin/lesson_10/lesson_10-task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10-task_2.kt
@@ -2,3 +2,18 @@ package lesson_10
 
 /** Created by Platon2025 */
 
+fun main() {
+    println("Введите логин :")
+    if (!validatePassword(readln())) return
+    println("Введите пароль :")
+    if (!validatePassword(readln())) return
+    println("Ваши данные приняты. Добро пожаловать!")
+}
+
+fun validatePassword(word: String): Boolean {
+    if (word.length < 4) {
+        println("Логин или пароль недостаточно длинные")
+        return false
+    }
+    return true
+}
