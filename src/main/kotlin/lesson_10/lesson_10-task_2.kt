85b6f624d121981e5dcce734605c7ab4da00255b
platon2025 <platon@enod.xyz>
2023-09-21 20:57:36 +0500

    KS-10-2 -- Password length validation

--- src/main/kotlin/lesson_10/lesson_10-task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10-task_2.kt
@@ -3,19 +3,16 @@ package lesson_10
 /** Created by Platon2025 */
 
 fun main() {
+    val minLength = 4
     println("Введите логин :")
-    if (!validatePassword(readln())) return
+    if (!validatePassword(readln(), minLength)) return
     println("Введите пароль :")
-    if (!validatePassword(readln())) return
+    if (!validatePassword(readln(), minLength)) return
     println("Ваши данные приняты. Добро пожаловать!")
 
 }
 
-fun validatePassword(word: String): Boolean {
-    val minLength = 4
-    if (word.length < minLength) {
-        println("Логин или пароль недостаточно длинные")
-        return false
-    }
-    return true
-}
+fun validatePassword(word: String, minLength: Int): Boolean = if (word.length < minLength) {
+    println("Логин или пароль недостаточно длинные")
+    false
+} else true
