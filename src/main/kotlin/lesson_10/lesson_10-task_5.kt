e8542c1036807c68c99dd217a4ca02590667e20b
platon2025 <platon@enod.xyz>
2023-09-21 10:35:47 +0500

    KS-10-5 -- Authorization - 3 in 1

--- src/main/kotlin/lesson_10/lesson_10-task_5.kt
+++ src/main/kotlin/lesson_10/lesson_10-task_5.kt
@@ -2,3 +2,59 @@ package lesson_10
 
 /** Created by Platon2025 */
 
+fun main() {
+    val loginMinLength = 4
+    val passwordLength = 6
+    var loginGiven: String
+    var passwordGiven: String
+    var accessCode: Int
+    var codeGiven: Int
+
+    println("Придумайте логин :")
+    val login = readln()
+    if (!validateLogin(login, loginMinLength)) return
+
+    val password = generatePassword(passwordLength)
+    println("Ваш пароль : $password")
+
+    do {
+        println("Введите логин :")
+        loginGiven = readln()
+        println("Введите пароль")
+        passwordGiven = readln()
+        if (loginGiven != login || passwordGiven != password) println("Введенные данные неверны, попробуйте еще раз")
+    } while (loginGiven != login || passwordGiven != password)
+    println("Проверка пароля пройдена!")
+
+    do {
+        accessCode = getAccessCode()
+        println("Ваш код доступа $accessCode")
+        println("Введите код :")
+        codeGiven = readln().toInt()
+        if (codeGiven != accessCode) println("Код введен неверно, попробуйте ещё раз")
+
+    } while (codeGiven != accessCode)
+
+    println("Авторизация пройдена успешно!")
+
+}
+
+fun validateLogin(login: String, loginMinLength: Int): Boolean {
+    if (login.length < loginMinLength) {
+        println("Логин должен быть не короче $loginMinLength символов")
+        return false
+    }
+    return true
+}
+
+fun generatePassword(length: Int): String {
+    val chars = ('a'..'z') + ('0'..'9')
+    var password = ""
+    for (i in 1..length) password += chars.random()
+    return password
+
+}
+
+fun getAccessCode(): Int {
+    return (1000..9999).random()
+}
