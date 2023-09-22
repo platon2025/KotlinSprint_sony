e6670331e68a78c5c0c970505ce4c74a17f45221
platon2025 <platon@enod.xyz>
2023-09-22 21:05:40 +0500

    KS-11-3 -- Class : Set of objects iteration

--- src/main/kotlin/lesson_11/Class3.kt
+++ src/main/kotlin/lesson_11/Class3.kt
@@ -2,14 +2,24 @@ package lesson_11
 
 /** Created by Platon2025 */
 
-class User3(val userId: Int, val userName: String, val userPic: String, var userStatus: String,) {
+class User3(
+    val userId: Int,
+    val userName: String,
+    val userPic: String,
+    var userStatus: String,
+) {
 
     fun showUserInfo() {
         println("$userPic : $userName : $userStatus")
     }
 }
 
-class Room3(val roomId: Int, val roomName: String, val roomCover: String, var users: MutableSet<User3>,) {
+class Room3(
+    val roomId: Int,
+    val roomName: String,
+    val roomCover: String,
+    var users: MutableSet<User3>,
+) {
 
     fun showRoomInfo() {
         println("$roomName : $roomCover\nusers: ")
