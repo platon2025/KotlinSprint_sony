5a04f46311cf31ac5e18edd7ccf75c3cdb71f59a
platon2025 <platon@enod.xyz>
2023-09-22 10:40:01 +0500

    KS-11-2 -- Class functions

--- src/main/kotlin/lesson_11/lesson_11-task_2.kt
+++ src/main/kotlin/lesson_11/lesson_11-task_2.kt
@@ -2,3 +2,17 @@ package lesson_11
 
 /** Created by Platon2025 */
 
+fun main() {
+    val user = User2(1011, "petrov", "123321", "petrov@bashirov.ru")
+    println("Добавьте информацию о себе: ")
+    user.updateBio()
+    user.printUserInfo()
+    var message = "Дорогой ${user.login},\n"
+    println("Введите текущий пароль: ")
+    if (user.changePassword()) message += "Ваш пароль был успешно изменен на: ${user.password}"
+    else message += "Ваш пароль не был изменен"
+    message += "\nС наилучшими пожеланиями,\nАдминистрация Галактики"
+    user.sendEmailToUser(message)
+
+}
+
