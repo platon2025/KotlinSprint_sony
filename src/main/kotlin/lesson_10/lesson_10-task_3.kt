b3b447f23e9e102103a1f9fb4d5c4a08fa6d6de1
platon2025 <platon@enod.xyz>
2023-09-22 20:29:18 +0500

    KS-10-3 -- Password from special chars

--- src/main/kotlin/lesson_10/lesson_10-task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10-task_3.kt
@@ -3,7 +3,7 @@ package lesson_10
 /** Created by Platon2025 */
 
 fun main() {
-    println("Введите желаемую длину пароля :")
+    println("Введите желаемую длину пароля:")
     println("Ваш пароль: ${generatePassword(readln().toInt())}")
 
 }
