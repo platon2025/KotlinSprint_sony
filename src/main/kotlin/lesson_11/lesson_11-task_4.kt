f9599a6ab70bd3016f73d43ed66a77d56888d5af
platon2025 <platon@enod.xyz>
2023-09-22 23:16:01 +0500

    KS-11-4 -- class Contact + methods

--- src/main/kotlin/lesson_11/lesson_11-task_4.kt
+++ src/main/kotlin/lesson_11/lesson_11-task_4.kt
@@ -2,3 +2,30 @@ package lesson_11
 
 /** Created by Platon2025 */
 
+fun main() {
+
+    val firstName = "First Name"
+    val secondName = "Second Name"
+    val faceTimeId = 1213141516
+    val phones = mutableMapOf(Pair("сотовый", 89999994242), Pair("рабочий", 89998884242))
+    val email = "mail@mail.ru"
+
+    val contact = Contact4(
+        firstName,
+        secondName,
+        "mouse.png",
+        phones,
+        faceTimeId,
+        email
+    )
+
+    contact.makeVoiceCallByPhone("сотовый")
+    contact.makeVideoCall()
+    contact.sendMessage()
+    contact.pinContact()
+    println(contact.isPinned)
+
+
+//    contact.icons.forEach{println(it.get())}
+
+}
