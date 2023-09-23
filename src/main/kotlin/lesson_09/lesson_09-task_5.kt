0990963b83153bd531ca9029defa7ce8a9ba7630
platon2025 <platon@enod.xyz>
2023-09-23 15:15:56 +0500

    KS-09-5 -- set.sorted(), replaceFirstChar

--- src/main/kotlin/lesson_09/lesson_09-task_5.kt
+++ src/main/kotlin/lesson_09/lesson_09-task_5.kt
@@ -5,19 +5,15 @@ package lesson_09
 fun main() {
     val qty = 5
     var input: String
-    val set: MutableSet<String> = mutableSetOf()
+    val ingredients: MutableSet<String> = mutableSetOf()
 
     println("Давайте введем $qty ингредиентов для вашего блюда")
 
-    for (i in 0 until qty) {
-        println("Введите ингредиент №${i + 1}")
-        input = readln().lowercase().trim()
-        while (!set.add(input)) {
-            println("Ингредиент \"$input\" уже есть в списке\nВведите другой:")
-            input = readln().lowercase().trim()
-        }
-    }
+    do {
+        println("Введите ингредиент :")
+        ingredients.add(readln().lowercase().trim())
+    } while (ingredients.size < qty)
 
-    println("${set.sorted().joinToString(", ").replaceFirstChar { it.uppercase() }}.")
+    println("${ingredients.sorted().joinToString(", ").replaceFirstChar { it.uppercase() }}.")
 
 }
