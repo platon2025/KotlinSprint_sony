3768fbea997188513ed369531c449a8d644a176e
platon2025 <platon@enod.xyz>
2023-09-24 22:04:06 +0500

    KS-12-2 -- Weather : Fields declared and initialized in the body of the class

--- src/main/kotlin/lesson_12/lesson_12-task_2.kt
+++ src/main/kotlin/lesson_12/lesson_12-task_2.kt
@@ -2,3 +2,23 @@ package lesson_12
 
 /** Created by Platon2025 */
 
+fun main() {
+    val weatherToday = Weather2(32, 22, false, 760)
+    val weatherTomorrow = Weather2(28, 18, true, 740)
+
+    weatherToday.printWeatherInfo()
+    weatherTomorrow.printWeatherInfo()
+
+}
+
+class Weather2(dayTemp: Int, nightTemp: Int, isRainy: Boolean, atmPressure: Int) {
+
+    var dayTemp = dayTemp
+    var nightTemp = nightTemp
+    var isRainy = isRainy
+    var atmPressure = atmPressure
+
+    fun printWeatherInfo() {
+        println("Дневная температура : $dayTemp\nНочная температура : $nightTemp\nДождь : $isRainy\nАтм.давление : $atmPressure\n")
+    }
+}
