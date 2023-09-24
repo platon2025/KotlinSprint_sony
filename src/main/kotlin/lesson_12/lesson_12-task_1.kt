d5c0299fde15e2db24c629e41e70ba3f15390337
platon2025 <platon@enod.xyz>
2023-09-24 21:50:50 +0500

    KS-12-1 -- Weather : empty constructor

--- src/main/kotlin/lesson_12/lesson_12-task_1.kt
+++ src/main/kotlin/lesson_12/lesson_12-task_1.kt
@@ -2,3 +2,27 @@ package lesson_12
 
 /** Created by Platon2025 */
 
+fun main() {
+    val weatherToday = Weather1()
+    val weatherTomorrow = Weather1()
+
+    weatherToday.dayTemp = 32
+    weatherToday.nightTemp = 22
+    weatherTomorrow.isRainy = true
+    weatherTomorrow.atmPressure = 740
+
+    weatherToday.printWeatherInfo()
+    weatherTomorrow.printWeatherInfo()
+}
+
+class Weather1() {
+
+    var dayTemp = 28
+    var nightTemp = 18
+    var isRainy = false
+    var atmPressure = 760
+
+    fun printWeatherInfo() {
+        println("Дневная температура : $dayTemp\nНочная температура : $nightTemp\nДождь : $isRainy\nАтм.давление : $atmPressure\n")
+    }
+}
