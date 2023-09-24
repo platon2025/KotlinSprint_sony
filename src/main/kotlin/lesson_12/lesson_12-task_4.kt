77e640103da8d4ee224a9f2debc23eb92559c774
platon2025 <platon@enod.xyz>
2023-09-24 22:33:54 +0500

    KS-12-4 -- Weather : init

--- src/main/kotlin/lesson_12/lesson_12-task_4.kt
+++ src/main/kotlin/lesson_12/lesson_12-task_4.kt
@@ -2,3 +2,24 @@ package lesson_12
 
 /** Created by Platon2025 */
 
+fun main() {
+    val weatherYesterday = Weather4(26, 17, 740, true)
+    val weatherToday = Weather4(30, 22, 760)
+    val weatherTomorrow = Weather4(28, 20, 765)
+
+}
+
+class Weather4(
+    val dayTemp: Int,
+    val nightTemp: Int,
+    val atmPressure: Int,
+    val isRainy: Boolean = false,
+) {
+    init {
+        printWeatherInfo()
+    }
+
+    fun printWeatherInfo() {
+        println("Дневная температура : $dayTemp\nНочная температура : $nightTemp\nДождь : $isRainy\nАтм.давление : $atmPressure\n")
+    }
+}
