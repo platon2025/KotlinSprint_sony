aea84e38ec11baf8447eea4aabc322f1902d1fe6
platon2025 <platon@enod.xyz>
2023-09-26 00:20:28 +0500

    KS-13-4 -- Dynamically adding contacts

--- src/main/kotlin/lesson_13/lesson_13-task_4.kt
+++ src/main/kotlin/lesson_13/lesson_13-task_4.kt
@@ -2,3 +2,72 @@ package lesson_13
 
 /** Created by Platon2025 */
 
+fun main() {
+    var contactList: MutableList<Contact4> = mutableListOf()
+
+    contactList.add(Contact4("Зинаида Прокофьевна", "89991005000", null))
+    contactList.add(Contact4("Джейсон Борн", "89017007700", "АО Фортуна"))
+    contactList.add(Contact4("Майк Вазовский", "89650545454", "Корпорация М"))
+
+    println("Телефонная книга:\n")
+    contactList.forEach {
+        it.printContact()
+    }
+
+    println("Добавление нового контакта в Телефонную книгу")
+    contactList += addContact()
+
+    println("Обновленная телефонная книга\n")
+    contactList.forEach {
+        it.printContact()
+    }
+}
+
+fun addContact(): MutableList<Contact4> {
+    var phoneNumber = ""
+    var name: String? = null
+    var company: String? = null
+    var extraContacts: MutableList<Contact4> = mutableListOf()
+
+    while (true) {
+        do {
+            println("Введите номер телефона:")
+            phoneNumber = readln().trim()
+            if (phoneNumber == "") println("Номер телефона является обязательным.")
+        } while (phoneNumber == "")
+
+        do {
+            println("Введите Имя:")
+            name = readln().trim()
+            if (name == "") {
+                name = null
+                println("Так как вы не ввели имя, название Компании является обязательным")
+            }
+
+            println("Введите название Компании:")
+            company = readln().trim()
+            if (company == "") {
+                company = null
+                if (name == null) println("Необходимо ввести хотя бы один параметр Имя или название Компании")
+            }
+
+        } while (name == null && company == null)
+
+        extraContacts += Contact4(name, phoneNumber, company)
+
+        println("Если хотите добавить новую запись, введите \"да\"")
+        if (!readln().trim().equals("да", true)) return extraContacts
+    }
+}
+
+class Contact4(
+    val name: String?,
+    val phoneNumber: String,
+    val company: String?,
+) {
+    fun printContact() {
+        println(
+            "Имя: ${name ?: "[не указано]"}\nНомер: $phoneNumber\nКомпания: ${company ?: "[не указано]"}\n"
+        )
+    }
+}
