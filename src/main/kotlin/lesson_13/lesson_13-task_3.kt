5cd0263c52689799048d779c4c0718b14018c988
platon2025 <platon@enod.xyz>
2023-09-26 00:29:21 +0500

    KS-13-3 -- ?: in println

--- src/main/kotlin/lesson_13/lesson_13-task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13-task_3.kt
@@ -6,8 +6,8 @@ fun main() {
     var contactList: MutableList<Contact3> = mutableListOf()
 
     contactList.add(Contact3("Зинаида Прокофьевна", 89991005000, null))
-    contactList.add(Contact3("Платон Щукин", 89017007700, "Яндекс"))
-    contactList.add(Contact3("Майк Вазовский", 89650545454, "Корпорация"))
+    contactList.add(Contact3("Джейсон Борн", 89017007700, "АО Фортуна"))
+    contactList.add(Contact3("Майк Вазовский", 89650545454, "Корпорация М"))
 
     contactList.forEach {
         it.printContact()
