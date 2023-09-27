100d931eaaf80de03c8ea3cf1e8a70adfb446870
platon2025 <platon@enod.xyz>
2023-09-27 07:32:45 +0500

    KS-13-5 -- catch NumberFormatException

--- src/main/kotlin/lesson_13/lesson_13-task_5.kt
+++ src/main/kotlin/lesson_13/lesson_13-task_5.kt
@@ -2,3 +2,16 @@ package lesson_13
 
 /** Created by Platon2025 */
 
+fun main() {
+    val number: Long
+    println("Введите номер телефона:")
+    val input = readln()
+
+    number = try {
+        input.toLong()
+    } catch (e: NumberFormatException) {
+        println("Номер может содержать только цифры !")
+        0
+    }
+    println(number)
+}
