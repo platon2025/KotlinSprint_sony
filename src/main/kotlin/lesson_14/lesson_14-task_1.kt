3eb574250ee8ff3edcfa002a83038957f1659743
platon2025 <platon@enod.xyz>
2023-09-27 12:27:19 +0500

    KS-14-1 -- Standard ship

--- src/main/kotlin/lesson_14/lesson_14-task_1.kt
+++ src/main/kotlin/lesson_14/lesson_14-task_1.kt
@@ -2,3 +2,18 @@ package lesson_14
 
 /** Created by Platon2025 */
 
+fun main() {
+    val ship = StandardShip()
+    ship.showSpeed()
+    ship.showCapacity()
+}
+
+class StandardShip(val type: String = "Стандартный", val capacity: Int = 120, val speed: Int = 24) {
+    fun showSpeed() {
+        println("Скорость корабля $type - $speed узлов")
+    }
+
+    fun showCapacity() {
+        println("Грузоподьемность корабля $type - $capacity ящиков")
+    }
+}
